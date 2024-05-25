8111c218b4d23642f3e8c5525a472e89bc578f22
dungbun31 <dungbunmaxngau@gmail.com>
2024-05-25 10:08:43 +0300

    fix: fix update

--- student-management/src/components/UpdateStudent.js
+++ student-management/src/components/UpdateStudent.js
@@ -4,10 +4,12 @@ const UpdateStudent = ({ student, updateStudent, setShowUpdatePage }) => {
   const [name, setName] = useState(student.name);
   const [gender, setGender] = useState(student.gender);
   const [university, setUniversity] = useState(student.university);
+  const [age, setAge] = useState(student.age);
+  const [email, setEmail] = useState(student.email);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    updateStudent(student.id, { name, gender, university });
+    updateStudent(student.id, { name, age, email, gender, university });
   };
 
   return (
@@ -19,6 +21,18 @@ const UpdateStudent = ({ student, updateStudent, setShowUpdatePage }) => {
           placeholder="Name"
           value={name}
           onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Age"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <select value={gender} onChange={(e) => setGender(e.target.value)}>
           <option value="male">Male</option>
